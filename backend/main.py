da890acae7df39d31bf6ee01033a6be600260c9a
subin <github@subin.kr>
2024-07-24 09:47:57 +0900

    ADD: FastAPI example

---
+++ backend/main.py
@@ -0,0 +1,17 @@
+from typing import Union
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
