9ddf4df1adb5cbfe007df9b559e9b631c1cb97c5
subin <github@subin.kr>
2024-07-26 13:43:56 +0900

    FIX: Make API example

--- backend/main.py
+++ backend/main.py
@@ -5,13 +5,24 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+class Genres(BaseModel):
+    positive: list
+    negative: list
+    
+class Movie(BaseModel):
+    title: str
+    rating: str
+    score: float
+    genres: list
+    description: str
+    director: str
+    cast: str
 
-class Item(BaseModel):
-    name: str
-    price: float
-    is_offer: Union[bool, None] = None
+@app.get("/top20")
+def read_item() -> list:
+    return []
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.put("/recommend")
+def update_item(genres: Genres) -> list:
+    print(genres.positive, genres.negative)
+    return []
